ac6836b2deb2fb9a9bcdd771f5979cc1c8ce984f
srujal <137897097+SrujalDhanani@users.noreply.github.com>
2024-10-13 17:39:43 +0530

    changes

--- src/Test.js
+++ src/Test.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CharacterCard from "./CharacterCard";
-import Paggination from "./Paggination"; 
-import Loader from "./Loader";
+import Paggination from "./Paggination";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box"; // For centering the loader
 
 const API_URL = "https://swapi.dev/api/people/";
 
@@ -20,6 +21,7 @@ function Test() {
       setError(null);
       try {
         const response = await axios.get(`${API_URL}?page=${currentPage}`);
+        console.log("hello", response.data)
         setCharacters(response.data.results);
         setTotalPages(Math.ceil(response.data.count / itemsPerPage)); // Calculate total pages
       } catch (err) {
@@ -34,28 +36,42 @@ function Test() {
 
   const handleItemsPerPageChange = (event) => {
     setItemsPerPage(parseInt(event.target.value, 10));
-    setCurrentPage(1); 
+    setCurrentPage(1);
   };
 
   return (
     <div>
-      <h1 className="List_per">Star Wars Characters--</h1>
-      {loading && <Loader />}
-      {error && <p>{error}</p>}
-      <div className="character-grid">
-        {characters.map((character) => (
-          <CharacterCard key={character.name} character={character} />
-        ))}
-      </div>
-      <div className='Pagination'>
-        <Paggination
-          totalPages={totalPages}
-          itemsPerPage={itemsPerPage}
-          currentPage={currentPage}
-          onPageChange={setCurrentPage}
-          onItemsPerPageChange={handleItemsPerPageChange}
-        />
-      </div>
+      {loading ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <>
+          <h1 className="List_per">Star Wars Characters--</h1>
+          {error && <p>{error}</p>}
+          <div className="character-grid">
+            {characters.map((character) => (
+              <CharacterCard key={character.name} character={character} />
+            ))}
+          </div>
+          <div className="Pagination">
+            <Paggination
+              totalPages={totalPages}
+              itemsPerPage={itemsPerPage}
+              currentPage={currentPage}
+              onPageChange={setCurrentPage}
+              onItemsPerPageChange={handleItemsPerPageChange}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
